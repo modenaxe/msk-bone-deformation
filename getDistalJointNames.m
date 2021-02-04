7fbaf1725e4d344ef42e72c178880be4dd49bf36
Luca Modenese <luca.modenese@gmail.com>
2021-02-04 17:13:04 +0000

    given a body, returns all joints of which that bone is parent

---
+++ getDistalJointNames.m
@@ -0,0 +1,25 @@
+function distalJointSetNames = getDistalJointNames(osimModel, bodyName)
+
+% extract all joints
+modelJointSet = osimModel.getJointSet();
+N_j = modelJointSet.getSize();
+
+% counter of distal joints
+n_d = 1;
+
+for n_j = 0:N_j-1
+    
+    % get parent body name for each joint
+    jointParentName = char(modelJointSet.get(n_j).getParentBody().getName());
+    
+    % when matching with bodyName save name
+    if strcmp(jointParentName, bodyName)
+        % save it
+        distalJointSetNames(n_d) = {char(modelJointSet.get(n_j).getName())};
+        n_d = n_d + 1;
+    end
+end
+
+% display(['Distal joint of ',bodyName, ' is ', char(modelJointSet.get(n_j).getName())]);
+
+end
